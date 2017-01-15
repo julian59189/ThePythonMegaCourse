04579ca26604ebbd347a943816f62d762a61e2d2
julian59189 <julianeiler@gmx.de>
2017-01-15 17:58:51 +0100

    added buttons to hide feature locations

--- script10.py
+++ script10.py
@@ -3,7 +3,7 @@ import pandas
 
 df = pandas.read_csv("Volcanoes-USA.txt")
 map=folium.Map(location=[df['LAT'].mean(),df['LON'].mean()],
-    zoom_start=5, tiles='Stamen Terrain')
+    zoom_start=5, tiles='Mapbox bright')
 
 def color(elev):
     minimum = int(min(df['ELEV']))
@@ -17,12 +17,18 @@ def color(elev):
         col = 'red'
     return col
 
+fg = folium.FeatureGroup(name = "Volcano Locations")
 
 for lat,lon,name,elev in zip(df['LAT'],df['LON'],df['NAME'],df['ELEV']):
-    map.add_child(folium.Marker(location=[lat, lon], popup=name,
+    fg.add_child(folium.Marker(location=[lat, lon], popup=name,
         icon=folium.Icon(color=color(elev), icon_color='green')))
+
+map.add_child(fg)
 
 map.add_child(folium.GeoJson(data=open('World_population.json'),
     name = 'World Population',
     style_function=lambda x:{'fillColor':'green' if x['properties']['POP2005'] <= 10000000 else 'orange' if 10000000 < x['properties']['POP2005'] < 20000000 else 'red'}))
+
+map.add_child(folium.LayerControl())
+
 map.save(outfile='map_with_markers.html')
