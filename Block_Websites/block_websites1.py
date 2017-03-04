45b28fd37f6e8487ff6b42eda6bed97df00424f4
julian59189 <julianeiler@gmx.de>
2017-03-04 16:36:13 +0100

    added blocking website file

---
+++ Block_Websites/block_websites1.py
@@ -0,0 +1,13 @@
+import time
+from datetime import datetime as dt
+
+hosts_path="C:\Windows\System32\drivers\etc\hosts"
+redirect="127.0.0.1"
+websites_list=["www.facebook.com", "facebook.com"]
+
+while True:
+    if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 17):
+        print("Working hours")
+    else:
+        print("Fun hours...")
+    time.sleep(5)
